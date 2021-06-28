efbcdaf7a3c4508c64ebf81c7e4406f4705691aa
David548219 <34396308+David548219@users.noreply.github.com>
2021-06-28 16:36:27 +0300

    Added pymongo

--- odc_server/__init__.py
+++ odc_server/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flasgger import Swagger
+from pymongo import MongoClient
 
 app = Flask(__name__)
 swag = Swagger(app)
+db = MongoClient().odc
 
 from . import controllers
