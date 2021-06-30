607536b349a54ce811d59201d06f34addc95db47
David548219 <34396308+David548219@users.noreply.github.com>
2021-06-30 13:15:12 +0300

    Added banknote issuing

--- odc_server/controllers/misc.py
+++ odc_server/controllers/misc.py
@@ -1,9 +1,12 @@
-from flask import request
-from flasgger import swag_from
+from datetime import datetime
 
 import rsa
+from bson import ObjectId
+from flasgger import swag_from
+from flask import request
 
-from odc_server import app, db
+from odc_server import app, db, bpk
+from odc_server.crypto import hash_items, sign_with_private_key
 
 
 @app.route("/regw", methods=["POST"])
@@ -21,3 +24,54 @@ def register_wallet():
 
     wid = db.wallets.insert_one({"sok": sok}).inserted_id
     return {"id": 200, "wid": str(wid)}
+
+
+@app.route("/issb", methods=["POST"])
+@swag_from("apidocs/issb.yml")
+def issue_banknotes():
+    wid = request.form["wid"]
+    amount = request.form["amount"]
+
+    if (not amount.isnumeric()) or int(amount) < 1:
+        return {"id": 400, "message": "Amount should be a non-zero integer"}, 400
+
+    if len(wid) != 24:
+        return {"id": 400, "message": "wid should be 24 char hex string"}, 400
+
+    try:
+        int(wid, 16)
+    except:
+        return {"id": 400, "message": "wid should be 24 char hex string"}, 400
+
+    wallet = db.wallets.find_one({"_id": ObjectId(wid)})
+    if wallet is None:
+        return {"id": 400, "message": "wid not registered"}, 400
+
+    amount = int(amount)
+    give_amounts = dict()
+    banknote_amounts = [5000, 2000, 1000, 500, 100, 50, 10, 5, 2, 1]
+    for banknote_amount in banknote_amounts:
+        banknote_count = amount // banknote_amount
+        amount = amount % banknote_amount
+        if banknote_count > 0:
+            give_amounts[banknote_amount] = banknote_count
+        if amount == 0:
+            break
+
+    current_time = datetime.now().strftime("%Y%m%d%H%M%S%f")
+    given_banknotes = list()
+    for banknote_amount, banknote_count in give_amounts.items():
+        banknote_template = {"code": 643, "time": current_time, "amount": banknote_amount}
+        for _ in range(banknote_count):
+            banknote = dict(banknote_template)
+
+            inserted_id = db.banknotes.insert_one(dict(banknote)).inserted_id
+
+            banknote_hash = hash_items(banknote.values())
+            banknote["signature"] = sign_with_private_key(banknote_hash, bpk)
+
+            db.banknotes.update_one({"_id": inserted_id}, {"$set": {"signature": banknote["signature"]}})
+
+            given_banknotes.append(banknote)
+
+    return {"id": 200, "issued_banknotes": given_banknotes}
