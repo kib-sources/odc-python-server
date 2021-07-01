29545b7092bf4fb3323e3f932765611f5ddbd8c8
David548219 <34396308+David548219@users.noreply.github.com>
2021-07-01 20:44:42 +0300

    bpk and bok are fetched from files

--- odc_server/__init__.py
+++ odc_server/__init__.py
@@ -1,23 +1,25 @@
 import os
+import sys
 
-from flask import Flask
 from flasgger import Swagger
+from flask import Flask
 from pymongo import MongoClient
 
-bok = """-----BEGIN RSA PUBLIC KEY-----
-MEgCQQCZScdB8AFwcrZDOLVsBT7m+KyuARWixZCstV99oOMYD318o0rhAqSYk/3Q
-nxV31GMYcJv7qABEqnowEkTGDh1TAgMBAAE=
------END RSA PUBLIC KEY-----"""
+bok_location = os.getenv("bok_location")
+bpk_location = os.getenv("bpk_location")
+if bok_location is None or bpk_location is None:
+    print("bpk/bok location not provided", file=sys.stderr)
+    sys.exit()
 
-bpk = """-----BEGIN RSA PRIVATE KEY-----
-MIIBPQIBAAJBAJlJx0HwAXBytkM4tWwFPub4rK4BFaLFkKy1X32g4xgPfXyjSuEC
-pJiT/dCfFXfUYxhwm/uoAESqejASRMYOHVMCAwEAAQJBAJTydLSkgrGCNYpSEy9Y
-VYvXfOtDUIOul2rKfnQzHWRQWX8MntoQTdw30v2gQuDI8gEpBqP4llYOM8ws4BTb
-sykCIwC4TePUAVtG0TME+7GE4Ont6iOkAq3WpqV7M9M062Vd/JTtAh8A1OsgEVsk
-g7bwB1lLa5TT8gR1tOV0oGB31KI9G3M/AiMAjK/QaOY8MdvBcV1cDg3OJDGl0S3G
-W2NMULan0+6Yq10CpQIeLhj952QRQscfrqehkZg2TwayKUkod/SK3SmHC2NnAiI+
-g4WPPFxHv/30FacTi1BTf2HD32FQ3KtKdu+hi6v7JeLq
------END RSA PRIVATE KEY-----"""
+try:
+    bok_file = open(bok_location, "r")
+    bok = bok_file.read()
+
+    bpk_file = open(bpk_location, "r")
+    bpk = bpk_file.read()
+except:
+    print("Failed to read bpk/bok from file", file=sys.stderr)
+    sys.exit()
 
 mongo_uri = os.getenv("mongo_uri")
 if mongo_uri is None:
