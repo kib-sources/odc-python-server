e20bc1fdf732a498139d555f1a1d99138c07e0ec
David548219 <34396308+David548219@users.noreply.github.com>
2021-07-01 16:17:58 +0300

    Added random numerical string generator

--- odc_server/utils.py
+++ odc_server/utils.py
@@ -1,3 +1,7 @@
+import random
+import string
+
+
 def is_hex_24(a: str) -> bool:
     if len(a) != 24:
         return False
@@ -6,3 +10,7 @@ def is_hex_24(a: str) -> bool:
     except:
         return False
     return True
+
+
+def random_numerical_string(l):
+    return ''.join(random.SystemRandom().choice(string.digits) for _ in range(l))
