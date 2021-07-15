e95ad2c4cc3705c42bd68d7eaeb17cb36ebb2dd5
David548219 <34396308+David548219@users.noreply.github.com>
2021-07-15 22:21:08 +0300

    wallet_signature -> transaction_signature

--- odc_server/controllers/transfer.py
+++ odc_server/controllers/transfer.py
@@ -21,7 +21,7 @@ def receive_banknote():
     time = request_json["time"]
     otok = request_json["otok"]
     otok_signature = request_json["otok_signature"]
-    wallet_signature = request_json["wallet_signature"]
+    transaction_signature = request_json["transaction_signature"]
 
     if not is_hex(bnid, 24):
         return {"code": 400, "message": "bnid should be 24 char hex string"}, 400
@@ -56,13 +56,12 @@ def receive_banknote():
     if not verify_time_is_near_current(time, 60):
         return {"code": 400, "message": "time should be within [current - 60, current] (Unix Epoch time)"}, 400
 
-    if not is_hex(wallet_signature, 128):
-        return {"code": 400, "message": "wallet_signature should be 128 char hex string"}, 400
+    if not is_hex(transaction_signature, 128):
+        return {"code": 400, "message": "transaction_signature should be 128 char hex string"}, 400
 
     transaction_hash = hash_items([uuid, otok, bnid, time])
-    print([uuid, otok, bnid, time])
-    if not verify_with_public_key(transaction_hash, wallet_signature, sok):
-        return {"code": 401, "message": "Invalid wallet signature"}, 401
+    if not verify_with_public_key(transaction_hash, transaction_signature, sok):
+        return {"code": 401, "message": "Invalid transaction signature"}, 401
 
     magic = random_numerical_string(16)
     transaction_hash_signed = sign_with_private_key(transaction_hash, bpk)
