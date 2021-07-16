283969c20215cc41e60dccc914e7b8142c96f5c4
David548219 <34396308+David548219@users.noreply.github.com>
2021-07-16 20:15:24 +0300

    Переименования параметров

--- odc_server/utils.py
+++ odc_server/utils.py
@@ -3,18 +3,18 @@ import string
 import time
 
 
-def is_hex(a: str, l: int) -> bool:
-    if len(a) != l:
+def is_hex(string_to_check: str, hex_string_length: int) -> bool:
+    if len(string_to_check) != hex_string_length:
         return False
     try:
-        int(a, 16)
+        int(string_to_check, 16)
     except:
         return False
     return True
 
 
-def random_numerical_string(l):
-    return ''.join(random.SystemRandom().choice(string.digits) for _ in range(l))
+def random_numerical_string(numerical_string_length):
+    return ''.join(random.SystemRandom().choice(string.digits) for _ in range(numerical_string_length))
 
 
 def current_epoch_time():
